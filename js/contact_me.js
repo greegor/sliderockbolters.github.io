889842d49a38e477239c47e6883954f0673186ec
greegor <greegor@users.noreply.github.com>
2020-02-18 08:11:11 -0700

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,6 +17,40 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
+                url: "https://formspree.io//myywqqzy",
+                method: "POST",
+                data: {message: message, 
+                       name: name,
+                       _replyto: email, 
+                       //_cc: "greg@softwareodyssey.com",
+                       _subject: "Message from Slide-Rock Bolters website"},
+                dataType: "json",
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            });
+           /*$.ajax({
                 url: "mail/contact_me.php",
                 type: "POST",
                 //url: "././mail/send_contact.php",
@@ -50,7 +84,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            })
+            })*/
         },
         filter: function() {
             return $(this).is(":visible");
